e314197096fd2035cb43c50940d15492ea494666
Karanraj Chauhan <chauhank@bu.edu>
2017-04-26 17:30:20 -0400

    Initial state machine implementation

--- main.c
+++ main.c
@@ -7,10 +7,11 @@
 #include <ti/devices/msp432p4xx/inc/msp.h>
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
-#define STATE1 1
-#define STATE2 2
-#define STATE3 3
-#define STATE4 4
+#define INIT_STATE 1    // nothing received so far
+#define AA_RECEIVED 2    // one aa received
+#define AA55_RECEIVED 3    // aa55 received
+#define AA55AA_RECEIVED 4    // aa55aa received
+#define AA55AA55_RECEIVED 5    // aa55aa55 received
 
 volatile uint8_t currentState = 0;
 
@@ -18,14 +19,14 @@ volatile uint8_t currentState = 0;
 volatile uint8_t rxData[12];
 volatile uint8_t index = 0;
 
-volatile uint8_t endWords[4];
-volatile uint8_t wordIndex = 0;
+//volatile uint8_t endWords[4];
+//volatile uint8_t wordIndex = 0;
 
 volatile uint16_t xpos;
 volatile uint16_t ypos;
 
-const uint16_t rightThreshold = 213;
-const uint16_t leftThreshold = 106;
+const uint16_t RIGHT_THRESHOLD = 213;
+const uint16_t LEFT_THRESHOLD = 106;
 
 /* received object format
     0, 1     y              sync: 0xaa55=normal object, 0xaa56=color code object
@@ -53,7 +54,6 @@ const eUSCI_UART_Config uartConfig =
 
 void main(void)
 {
-	
     // Stop the watch dog timer
     MAP_WDT_A_holdTimer();
 
@@ -75,7 +75,6 @@ void main(void)
 
     /* Enabling interrupts */
     MAP_UART_enableInterrupt(EUSCI_A1_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
-//    MAP_UART_enableInterrupt(EUSCI_A1_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT);
     MAP_Interrupt_enableInterrupt(INT_EUSCIA1);
     MAP_Interrupt_enableSleepOnIsrExit();
     MAP_Interrupt_enableMaster();
@@ -84,38 +83,66 @@ void main(void)
     __no_operation();
 }
 
-
+/* ISR for UART receive */
 void EUSCIA1_IRQHandler(void)
 {
-    uint_fast8_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A1_BASE);  // interrupt status
+    // interrupt status
+    uint_fast8_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A1_BASE);
 
-    if (status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
+    if (status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)   // check if receive flag is raised
     {
         uint8_t data = MAP_UART_receiveData(EUSCI_A1_BASE);
-//        if (data)
-        {
-            if ((endWords[0]==0xaa && endWords[1]==0x55 && endWords[2]==0xaa && endWords[3]==0x55))
-            {
-                if (index==12)
-                {
-//                    wordIndex = 0;
-                    endWords[0] = data;
-                    endWords[1] = 0;
-                    endWords[2] = 0;
-                    endWords[3] = 0;
-                }
-                else
-                {
-                    rxData[index++] = data;
-                }
-            }
-            else
-            {
-                if (wordIndex==4) wordIndex = 0;
-                endWords[wordIndex++] = data;
-            }
 
+        if (currentState==INIT_STATE && data==0xaa) // nothing received yet
+        {
+            currentState = AA_RECEIVED;
         }
+        else if (currentState==AA_RECEIVED && data==0xa55)    // 'aa' already received
+        {
+            currentState = AA55_RECEIVED;
+        }
+        else if (currentState==AA55_RECEIVED && data==0xaa)    // 'aa55' already received
+        {
+            currentState = AA55AA_RECEIVED;
+        }
+        else if (currentState==AA55AA_RECEIVED && data==0x55)
+        {
+            currentState = AA55AA55_RECEIVED;
+        }
+        else if (currentState==AA55AA55_RECEIVED)
+        {
+            rxData[++index] = data;
+            if (index==12)
+            {
+                currentState = INIT_STATE;
+            }
+        }
+
+
+////        if (data)
+//        {
+//            if ((endWords[0]==0xaa && endWords[1]==0x55 && endWords[2]==0xaa && endWords[3]==0x55))
+//            {
+//                if (index==12)
+//                {
+////                    wordIndex = 0;
+//                    endWords[0] = data;
+//                    endWords[1] = 0;
+//                    endWords[2] = 0;
+//                    endWords[3] = 0;
+//                }
+//                else
+//                {
+//                    rxData[index++] = data;
+//                }
+//            }
+//            else
+//            {
+//                if (wordIndex==4) wordIndex = 0;
+//                endWords[wordIndex++] = data;
+//            }
+//
+//        }
 
     }
 }
